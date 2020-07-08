23893999affcc537e73509c2183fa6b513361526
allancolibao <colibaoallanreyes@gmail.com>
2020-07-08 13:57:19 +0800

    commit 07-08-2020 comment route

--- routes/web.php
+++ routes/web.php
@@ -13,24 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Landing page
 Route::get('/', 'MainController@index')->name('index');
+
+// Search function 
 Route::post('/search', 'MainController@search')->name('search');
+
+// Updating record 
 Route::get('/edit/{id}', 'MainController@edit')->name('edit');
 Route::post('/update/{id}/{eacode}', 'MainController@update')->name('update');
 Route::post('/bulk-update', 'MainController@bulkUpdate')->name('bulk.update');
 Route::get('/eacode/{eacode}', 'MainController@eacodeView')->name('eacode');
+
+// Transmission page
 Route::get('/transmit', 'MainController@transmit')->name('transmit');
 Route::post('/trans', 'MainController@trans')->name('trans');
+
+// Insert IYCF record
 Route::get('/add', 'MainController@add')->name('add');
 Route::post('/insert', 'MainController@insert')->name('insert');
 
+// Updating remark pages
 Route::get('/remarks', 'MainController@remarks')->name('remarks');
 Route::get('/update/remarks', 'MainController@updateRemarks')->name('update.remarks');
 
+// Export and import
 Route::get('/export', 'MainController@export')->name('export');
 Route::post('/import', 'MainController@import')->name('import');
-Route::get('/delete', 'MainController@delete')->name('delete');
 Route::post('/data/export', 'MainController@exportData')->name('data.export');
 
+// Delete the uploaded csv data
+Route::get('/delete', 'MainController@delete')->name('delete');
+
+// Transmission page
 Route::get('/trans', 'MainController@transmission')->name('transmission');
 Route::get('/diff/{ea}', 'MainController@diff')->name('diff');
